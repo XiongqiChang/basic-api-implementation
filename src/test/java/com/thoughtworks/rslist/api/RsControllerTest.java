055bd3e3c4cb2d480286c84ad37179c32a18e563
XiongqiChang <2838159948@qq.com>
2020-09-15 14:49:56 +0800

    完成热搜接口

---
+++ src/test/java/com/thoughtworks/rslist/api/RsControllerTest.java
@@ -0,0 +1,33 @@
+package com.thoughtworks.rslist.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+/**
+ * @Author: xqc
+ * @Date: 2020/9/15 - 09 - 15 - 14:41
+ * @Description: com.thoughtworks.rslist.api
+ * @version: 1.0
+ */
+@SpringBootTest
+@AutoConfigureMockMvc
+class RsControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void should_get_list() throws Exception {
+        mockMvc.perform(get("/rs/list")).andExpect(content().string("[第一条事件, 第二条事件, 第三条事件]"))
+                .andExpect(status().isOk());
+    }
+
+}
