6d590883ebebd58513a7792267a95db4c45a9683
XiongqiChang <2838159948@qq.com>
2020-09-16 17:41:02 +0800

    完成返回类型代码

--- src/main/java/com/thoughtworks/rslist/api/RsController.java
+++ src/main/java/com/thoughtworks/rslist/api/RsController.java
@@ -8,8 +8,7 @@ import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
+
 import java.util.ArrayList;
 import java.util.List;
 
